1627b057c46e2b283cf78e058eb92d26e94fe4a9
Edward P. Kelly <macromediaprod@yahoo.com>
2016-02-11 17:29:13 -0500

    Building tapper controls

--- 2_29_Tapper-Extreme/2_29_Tapper-Extreme/ViewController.swift
+++ 2_29_Tapper-Extreme/2_29_Tapper-Extreme/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Props
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    // Outlets
     @IBOutlet weak var logoImage:UIImageView!
     @IBOutlet weak var tapCountTxt:UITextField!
     @IBOutlet weak var playButton:UIButton!
@@ -18,13 +23,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var tapsLabel:UILabel!
     
     @IBAction func onPlayTapped(sender: UIButton!) {
-        logoImage.hidden = true
-        playButton.hidden = true
-        tapCountTxt.hidden = true
-        tapButton.hidden = false
-        tapsLabel.hidden = false
+        
+        if tapCountTxt.text != "" && tapCountTxt.text != nil {
+            logoImage.hidden = true
+            playButton.hidden = true
+            tapCountTxt.hidden = true
+            tapButton.hidden = false
+            tapsLabel.hidden = false
+            
+            maxTaps = Int(tapCountTxt.text!)!
+            currentTaps = 0
+            
+            updateTapsLabel()
+        }
     }
     
+    @IBAction func onCoinTapped(sender: UIButton!) {
+        currentTaps++
+        updateTapsLabel()
+        
+        if isGameOver() {
+            restartGame()
+        }
+    }
+    
+    private func isGameOver() -> Bool {
+        return currentTaps >= maxTaps
+    }
+    
+    private func updateTapsLabel() {
+        tapsLabel.text = "\(currentTaps) Taps"
+    }
+    
+    private func restartGame() {
+        maxTaps = 0
+        tapCountTxt.text = ""
+        
+        logoImage.hidden = false
+        playButton.hidden = false
+        tapCountTxt.hidden = false
+        tapButton.hidden = true
+        tapsLabel.hidden = true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
