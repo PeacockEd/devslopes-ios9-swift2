0de43e3cb22e5261fbec3861be28239030caa959
Edward P. Kelly <macromediaprod@yahoo.com>
2016-02-12 11:51:23 -0500

    Finishing up Multiples exercise

--- 2_30_Multiples/2_30_Multiples/ViewController.swift
+++ 2_30_Multiples/2_30_Multiples/ViewController.swift
@@ -9,6 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var multipleValue = 0
+    var currentSum = 0
+    
+    @IBOutlet weak var multiplesText:UITextField!
+    @IBOutlet weak var logoImage:UIImageView!
+    @IBOutlet weak var playButton:UIButton!
+    
+    @IBOutlet weak var sumLabel:UILabel!
+    @IBOutlet weak var addButton:UIButton!
+    
+    @IBAction func onTapPlay(sender:UIButton!) {
+        if multiplesText.text != nil &&
+            multiplesText.text != "" &&
+            (Int(multiplesText.text!) > 0)
+        {
+            logoImage.hidden = true
+            playButton.hidden = true
+            multiplesText.hidden = true
+            sumLabel.hidden = false
+            addButton.hidden = false
+            multipleValue = Int(multiplesText.text!)!
+        }
+    }
+    
+    @IBAction func onTapAdd(sender:UIButton!) {
+        sumLabel.text = "\(currentSum) + \(multipleValue) = \(calculateSum())"
+        currentSum = calculateSum()
+    }
+    
+    private func calculateSum() -> Int {
+        return currentSum + multipleValue
+    }
+    
+    private func restartGame() {
+        multipleValue = 0
+        currentSum = 0
+        sumLabel.text = "Press Add to Add!"
+        multiplesText.text = ""
+        logoImage.hidden = false
+        playButton.hidden = false
+        multiplesText.hidden = false
+        sumLabel.hidden = true
+        addButton.hidden = true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
